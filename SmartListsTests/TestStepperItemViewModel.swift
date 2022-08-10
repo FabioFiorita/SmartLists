c2a962810fc1f4e4070c03a3f4eafc6b11f69cb0
Fabio Fiorita <fabiolfp@gmail.com>
2022-08-10 18:48:00 -0300

    adjusting StepperItemViewModel unit test

--- SmartListsTests/TestStepperItemViewModel.swift
+++ SmartListsTests/TestStepperItemViewModel.swift
@@ -13,69 +13,93 @@ final class TestStepperItemViewModel: XCTestCase {
     
     var viewContext: NSManagedObjectContext!
     var stepperItemViewModel: StepperItemViewModel!
+    var listType: ListType!
     
     override func setUp() {
         super.setUp()
         self.viewContext = TestCoreDataStack.shared.container.viewContext
         self.stepperItemViewModel = StepperItemViewModel(viewContext: viewContext)
+        addListType(viewContext: viewContext)
     }
     
     override func tearDown() {
         flushData(viewContext: viewContext)
+        flushList(viewContext: viewContext)
         self.viewContext = nil
         self.stepperItemViewModel = nil
         super.tearDown()
     }
 
     func testAddItem() {
-        let newItem = stepperItemViewModel.addItem(content: "Test")
-        XCTAssertNotNil(newItem, "Object should not be nil")
-        XCTAssertEqual("Test", newItem.content)
-        XCTAssertTrue(newItem.amount == 0)
+        let newItem = stepperItemViewModel.addItem(content: "Test", listType: listType)
+        XCTAssertNotNil(newItem, "Item should not be nil")
+        XCTAssertEqual("Test", newItem.content, "Item should have the same content")
+        XCTAssertTrue(newItem.listType == listType, "Item should have the same list type")
+        XCTAssertTrue(newItem.amount == 0, "Item should have amount == 0")
         
     }
     
     func testIncreaseAmount() {
-        let newItem = stepperItemViewModel.addItem(content: "Test")
+        let newItem = stepperItemViewModel.addItem(content: "Test", listType: listType)
+        XCTAssertTrue(newItem.amount == 0, "Item should have amount == 0")
         let resItem = stepperItemViewModel.increaseAmount(item: newItem)
-        XCTAssertEqual(1, resItem.amount)
+        XCTAssertEqual(1, resItem.amount, "Item should have amount == 1 after increasing amount")
     }
     
     func testDecreaseAmount() {
-        let newItem = stepperItemViewModel.addItem(content: "Test")
+        let newItem = stepperItemViewModel.addItem(content: "Test", listType: listType)
         newItem.amount = 3
+        XCTAssertEqual(3, newItem.amount, "Item should have amount == 3")
         let resItem = stepperItemViewModel.decreaseAmount(item: newItem)
-        XCTAssertEqual(2, resItem.amount)
+        XCTAssertEqual(2, resItem.amount, "Item should have amount == 2 after decreasing amount")
     }
     
     func testDeleteItem() {
         let fetchRequest: NSFetchRequest<StepperItem> = StepperItem.fetchRequest()
         var resItems = try! viewContext.fetch(fetchRequest)
-        XCTAssertTrue(resItems.isEmpty)
+        XCTAssertTrue(resItems.isEmpty, "FetchRequest should return an empty array")
         
-        let newItem = stepperItemViewModel.addItem(content: "Test")
+        let newItem = stepperItemViewModel.addItem(content: "Test", listType: listType)
         resItems = try! viewContext.fetch(fetchRequest)
-        XCTAssertFalse(resItems.isEmpty)
-        XCTAssertEqual(newItem, resItems.first)
+        XCTAssertFalse(resItems.isEmpty, "FetchRequest should return an array with elements")
+        XCTAssertEqual(newItem, resItems.first, "The first item in the array should be the same as the new item created")
         
         stepperItemViewModel.deleteItem(item: newItem)
         resItems = try! viewContext.fetch(fetchRequest)
-        XCTAssertTrue(resItems.isEmpty)
+        XCTAssertTrue(resItems.isEmpty, "FetchRequest should return an empty array after deletion")
     }
     
-//    func testUpdateItem() {
-//        
-//
-//    }
+    func testUpdateItem() {
+        let newItem = stepperItemViewModel.addItem(content: "Test", listType: listType)
+        let content = newItem.content
+        let modifiedItem = stepperItemViewModel.updateItem(item: newItem, newContent: "Test2")
+        XCTAssertEqual(newItem.id, modifiedItem.id, "Modified item should have the same id as the new item created")
+        XCTAssertNotEqual(content, modifiedItem.content, "Modified item should have a different content as the new item created")
+    }
 
 }
 
 extension TestStepperItemViewModel {
     func flushData(viewContext: NSManagedObjectContext) {
         let fetchRequest:NSFetchRequest<NSFetchRequestResult> = NSFetchRequest<NSFetchRequestResult>(entityName: "StepperItem")
-        let objs = try! viewContext.fetch(fetchRequest)
-        for case let obj as NSManagedObject in objs {
-            viewContext.delete(obj)
+        let items = try! viewContext.fetch(fetchRequest)
+        for case let item as NSManagedObject in items {
+            viewContext.delete(item)
+        }
+        try! viewContext.save()
+    }
+    func addListType(viewContext: NSManagedObjectContext) {
+        listType = ListType(context: viewContext)
+        listType.id = UUID()
+        listType.title = "Title"
+        listType.type = "Stepper List"
+        try! viewContext.save()
+    }
+    func flushList(viewContext: NSManagedObjectContext) {
+        let fetchRequest:NSFetchRequest<NSFetchRequestResult> = NSFetchRequest<NSFetchRequestResult>(entityName: "ListType")
+        let lists = try! viewContext.fetch(fetchRequest)
+        for case let list as NSManagedObject in lists {
+            viewContext.delete(list)
         }
         try! viewContext.save()
     }
