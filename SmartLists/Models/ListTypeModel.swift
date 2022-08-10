9246336dbe51dc9094e9369fde32198becae1c69
Fabio Fiorita <fabiolfp@gmail.com>
2022-08-10 18:49:14 -0300

    Creating ListTypeModel to use in a Picker element

---
+++ SmartLists/Models/ListTypeModel.swift
@@ -0,0 +1,13 @@
+//
+//  ListTypeModel.swift
+//  SmartLists
+//
+//  Created by Fabio Fiorita on 10/08/22.
+//
+
+import Foundation
+
+struct ListTypeModel: Hashable {
+    var name: String
+    var image: String
+}
