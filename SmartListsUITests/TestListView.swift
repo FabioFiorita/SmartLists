442279638bfbf9ebd1f32972117083cff8d8ac15
Fabio Fiorita <fabiolfp@gmail.com>
2022-08-17 20:24:30 -0300

    Creating UI Tests

---
+++ SmartListsUITests/TestListView.swift
@@ -0,0 +1,26 @@
+//
+//  TestListView.swift
+//  SmartListsUITests
+//
+//  Created by Fabio Fiorita on 15/08/22.
+//
+
+import XCTest
+
+final class TestListView: XCTestCase {
+    let app = XCUIApplication()
+
+    override func setUpWithError() {
+        continueAfterFailure = false
+    }
+
+    override func tearDownWithError() {
+        
+    }
+
+    func testNewItemButton() {
+        app.toolbars["Toolbar"]/*@START_MENU_TOKEN@*/.buttons["New list"]/*[[".otherElements[\"New list\"].buttons[\"New list\"]",".buttons[\"New list\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        let newItemModal = app.navigationBars["New list"]
+        XCTAssert(newItemModal.isEmpty == false)
+    }
+}
