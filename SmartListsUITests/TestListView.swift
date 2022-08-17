0cbe98a95004fbb02ace99292f6156927234133d
Fabio Fiorita <fabiolfp@gmail.com>
2022-08-17 20:52:58 -0300

    wip: Creating UI Tests

--- SmartListsUITests/TestListView.swift
+++ SmartListsUITests/TestListView.swift
@@ -10,17 +10,18 @@ import XCTest
 final class TestListView: XCTestCase {
     let app = XCUIApplication()
 
-    override func setUpWithError() {
+    override func setUpWithError() throws {
+        app.launch()
         continueAfterFailure = false
     }
 
-    override func tearDownWithError() {
+    override func tearDownWithError() throws {
         
     }
 
     func testNewItemButton() {
         app.toolbars["Toolbar"]/*@START_MENU_TOKEN@*/.buttons["New list"]/*[[".otherElements[\"New list\"].buttons[\"New list\"]",".buttons[\"New list\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
         let newItemModal = app.navigationBars["New list"]
-        XCTAssert(newItemModal.isEmpty == false)
+        XCTAssert(newItemModal.exists)
     }
 }
